6a0af00a2cbddfa7b3595f8871374c209fc04b40
Pierre-Olivier Boisvert <pierre-olivierboisvert@Pierre-Oliviers-Mac-mini.local>
2021-09-08 21:40:02 -0400

    Jest bug

--- testing/src/components/CommentBox.js
+++ testing/src/components/CommentBox.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from 'actions';
 
 class CommentBox extends Component {
   state = { comment: '' };
@@ -12,6 +14,7 @@ class CommentBox extends Component {
     e.preventDefault();
 
     // Call action
+    this.props.saveComment(this.state.comment);
 
     // Save data
 
@@ -32,4 +35,4 @@ class CommentBox extends Component {
   }
 }
 
-export default CommentBox;
+export default connect(null, actions)(CommentBox);
